936146f33bdec1b474774ad42f6a972bced546d5
DzKInShO2 <dzkinsho2@protonmail.com>
2024-07-28 22:01:56 +0800

    Change Background color

--- src/colorscheme.h
+++ src/colorscheme.h
@@ -1,7 +1,7 @@
 #ifndef _COLORSCHEME_H_
 #define _COLORSCHEME_H_
 
-#define GRID_COLOR (Color){ 215, 153, 33, 255 }
+#define GRID_COLOR (Color){ 60, 56, 54, 255 }
 #define BACKGROUND_COLOR (Color){ 40, 40, 40, 255 }
 #define SELECTION_COLOR (Color){ 251, 241, 199, 120 }
 
